3cb25f0df55ee1161ef238cb98528b5517b3fa2b
Angel <angel@HORO>
2017-03-28 18:23:40 -0700

    Inicio

--- PPO/Form1.Designer.cs
+++ PPO/Form1.Designer.cs
@@ -28,12 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pHerramientas = new System.Windows.Forms.Panel();
+            this.ssEstado = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.ssEstado.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pHerramientas
+            // 
+            this.pHerramientas.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.pHerramientas.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pHerramientas.Location = new System.Drawing.Point(0, 0);
+            this.pHerramientas.Name = "pHerramientas";
+            this.pHerramientas.Size = new System.Drawing.Size(991, 96);
+            this.pHerramientas.TabIndex = 0;
+            // 
+            // ssEstado
+            // 
+            this.ssEstado.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.ssEstado.Location = new System.Drawing.Point(0, 602);
+            this.ssEstado.Name = "ssEstado";
+            this.ssEstado.Size = new System.Drawing.Size(991, 22);
+            this.ssEstado.TabIndex = 1;
+            this.ssEstado.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 96);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(991, 506);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(991, 624);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.ssEstado);
+            this.Controls.Add(this.pHerramientas);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ssEstado.ResumeLayout(false);
+            this.ssEstado.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel pHerramientas;
+        private System.Windows.Forms.StatusStrip ssEstado;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
